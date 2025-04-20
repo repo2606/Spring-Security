4b06a8cb35e9fc6ab2fe5769cd0d85c243b78a7e
Deepak <130002300+repo2606@users.noreply.github.com>
2025-04-20 09:54:27 -0400

    Update SpringSecurityLatestApplication.java

--- src/main/java/com/javatechie/SpringSecurityLatestApplication.java
+++ src/main/java/com/javatechie/SpringSecurityLatestApplication.java
@@ -8,7 +8,7 @@ public class SpringSecurityLatestApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("welcome message +++changes");
+		System.out.println("chenages made in branch");
 		SpringApplication.run(
 
 
