d58a7ddaaceb0836de23ac5cee73f20a946a75d4
David <learn-git-fast@abc.com>
2025-04-20 09:32:39 -0400

    Initial commit

--- src/main/java/com/javatechie/SpringSecurityLatestApplication.java
+++ src/main/java/com/javatechie/SpringSecurityLatestApplication.java
@@ -7,7 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityLatestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityLatestApplication.class, args);
+
+		System.out.println("welcome message");
+		SpringApplication.run(
+
+
+				SpringSecurityLatestApplication.class, args);
 	}
 
 }
